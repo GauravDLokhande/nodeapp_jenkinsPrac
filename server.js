64c7a2ecdd22cb2c637f0e21260d2aeab3b189c0
Gaurav Lokhande <gauravbsl88@gmail.com>
2024-12-17 18:43:57 +0530

    5th commit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/',(request,response) => {
-	response.send('Muze Ghar Jaana Hai.........')
+	response.send('Job Dedo 4LPA ka.........')
 })
 
 app.listen(4000,'0.0.0.0',() => {
